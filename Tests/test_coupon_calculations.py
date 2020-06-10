efcccdf9138a89a8aba00828e7923f2fbb5a17e1
Paul Elsea <Paul.M.Elsea@gmail.com>
2020-06-10 15:12:58 -0500

    First set of price tests passing

--- Tests/test_coupon_calculations.py
+++ Tests/test_coupon_calculations.py
@@ -11,30 +11,17 @@ from store import coupon_calculations as coupon
 
 class MyTestCase(unittest.TestCase):
     def test_price_under_10_5_cash_10_percent(self):
-        assert(round(coupon.calculate_price(9.99, 5, 10), 2), 11.07)
+        assert round(coupon.calculate_price(14.99, 5, 10), 2) == 15.84
     def test_price_under_10_5_cash_15_percent(self):
-        assert(coupon.calculate_price(9.99, 5, 15), 90)
+        assert round(coupon.calculate_price(14.99, 5, 15), 2) == 15.31
     def test_price_under_10_5_cash_20_percent(self):
-        assert(coupon.calculate_price(9.99, 5, 20), 90)
+        assert round(coupon.calculate_price(14.99, 5, 20), 2) == 14.78
     def test_price_under_10_10_cash_10_percent(self):
-        assert(coupon.calculate_price(9.99, 10, 10), 90)
+        assert round(coupon.calculate_price(14.99, 10, 10), 2) == 11.07
     def test_price_under_10_10_cash_15_percent(self):
-        assert(coupon.calculate_price(9.99, 10, 15), 90)
+        assert round(coupon.calculate_price(14.99, 10, 15), 2) == 10.8
     def test_price_under_10_10_cash_20_percent(self):
-        assert(coupon.calculate_price(9.99, 10, 20), 90)
-
-    # def test_price_under_10_5_cash_10_percent(self):
-    #     assert(coupon.calculate_price(5, 5, 10), 90)
-    # def test_price_under_10_5_cash_15_percent(self):
-    #     assert(coupon.calculate_price(5, 5, 15), 90)
-    # def test_price_under_10_5_cash_20_percent(self):
-    #     assert(coupon.calculate_price(5, 5, 20), 90)
-    # def test_price_under_10_10_cash_10_percent(self):
-    #     assert(coupon.calculate_price(5, 10, 10), 90)
-    # def test_price_under_10_10_cash_15_percent(self):
-    #     assert(coupon.calculate_price(5, 10, 15), 90)
-    # def test_price_under_10_10_cash_20_percent(self):
-    #     assert(coupon.calculate_price(5, 10, 20), 90)
+        assert round(coupon.calculate_price(14.99, 10, 20), 2) == 10.54
 
 if __name__ == '__main__':
     unittest.main()
--- store/coupon_calculations.py
+++ store/coupon_calculations.py
@@ -5,10 +5,19 @@ Last Modified: 06/10/2020
 
 Program to ask which level of a subscription service they want to sign up for.
 """
-
+TAX_RATE = 1.06
 
 def calculate_price(price, cash_coupon, percent_coupon):
-    pass
+    global TAX_RATE
+    running_total = (price - cash_coupon) * ((100 - percent_coupon)/100)
+    if running_total <= 10.00:
+        final_total = (running_total + 5.95) * TAX_RATE
+        return final_total
 
 if __name__ == '__main__':
-    calculate_price()
+    print(round(calculate_price(14.99, 5, 10), 2))
+    print(round(calculate_price(14.99, 5, 15), 2))
+    print(round(calculate_price(14.99, 5, 20), 2))
+    print(round(calculate_price(14.99, 10, 10), 2))
+    print(round(calculate_price(14.99, 10, 15), 2))
+    print(round(calculate_price(14.99, 10, 20), 2))
