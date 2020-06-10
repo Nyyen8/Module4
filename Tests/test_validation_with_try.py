f5d334054f306637f833968bd1bbf377b5ad0e13
Paul Elsea <Paul.M.Elsea@gmail.com>
2020-06-10 17:08:32 -0500

    Validation third test fail

--- Tests/test_validation_with_try.py
+++ Tests/test_validation_with_try.py
@@ -26,10 +26,11 @@ class FunctionTestCase(unittest.TestCase):
                 avg.average()
         '''This test checks to see if the average func throws a valueError if the second entry is negative'''
 
-    # def test_average_negative_third_input(self):
-    #     with mock.patch('builtins.input', side_effect=[85, 90, -95]):
-    #         self.assertRaises(ValueError)
-    #     '''This test checks to see if the average func throws a valueError if the third entry is negative'''
-    #
+    def test_average_negative_third_input(self):
+        with mock.patch('builtins.input', side_effect=[85, 90, -95]):
+            with self.assertRaises(ValueError):
+                avg.average()
+        '''This test checks to see if the average func throws a valueError if the third entry is negative'''
+
 if __name__ == '__main__':
     unittest.main()
