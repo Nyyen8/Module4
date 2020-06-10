8ab6f3ea3417f502f577c643c0fa538f75be1524
Paul Elsea <Paul.M.Elsea@gmail.com>
2020-06-10 15:23:49 -0500

    Second set of price tests failing

--- Tests/test_coupon_calculations.py
+++ Tests/test_coupon_calculations.py
@@ -23,5 +23,18 @@ class MyTestCase(unittest.TestCase):
     def test_price_under_10_10_cash_20_percent(self):
         assert round(coupon.calculate_price(14.99, 10, 20), 2) == 10.54
 
+    def test_price_between_10_and_30_5_cash_10_percent(self):
+        assert round(coupon.calculate_price(29.99, 5, 10), 2) == 15.84
+    def test_price_between_10_and_30_5_cash_15_percent(self):
+        assert round(coupon.calculate_price(29.99, 5, 15), 2) == 15.31
+    def test_price_between_10_and_30_5_cash_20_percent(self):
+        assert round(coupon.calculate_price(29.99, 5, 20), 2) == 14.78
+    def test_price_between_10_and_30_10_cash_10_percent(self):
+        assert round(coupon.calculate_price(29.99, 10, 10), 2) == 11.07
+    def test_price_between_10_and_30_10_cash_15_percent(self):
+        assert round(coupon.calculate_price(29.99, 10, 15), 2) == 10.8
+    def test_price_between_10_and_30_10_cash_20_percent(self):
+        assert round(coupon.calculate_price(29.99, 10, 20), 2) == 10.54
+
 if __name__ == '__main__':
     unittest.main()
--- store/coupon_calculations.py
+++ store/coupon_calculations.py
@@ -6,13 +6,23 @@ Last Modified: 06/10/2020
 Program to ask which level of a subscription service they want to sign up for.
 """
 TAX_RATE = 1.06
+UNDER_10_SHIPPING = 5.95
+UNDER_30_SHIPPING = 7.95
+UNDER_50_SHIPPING = 11.95
 
 def calculate_price(price, cash_coupon, percent_coupon):
     global TAX_RATE
+    global UNDER_10_SHIPPING
+    global UNDER_30_SHIPPING
+    global UNDER_50_SHIPPING
+
     running_total = (price - cash_coupon) * ((100 - percent_coupon)/100)
     if running_total <= 10.00:
-        final_total = (running_total + 5.95) * TAX_RATE
+        final_total = (running_total + UNDER_10_SHIPPING) * TAX_RATE
         return final_total
+        if 30 > running_total >= 10:
+            final_total = (running_total + UNDER_30_SHIPPING) * TAX_RATE
+            return final_total
 
 if __name__ == '__main__':
     print(round(calculate_price(14.99, 5, 10), 2))
